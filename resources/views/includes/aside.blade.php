c2e29feda299dd064dc5b4a4cb5bad1fd2b3e28f
AlexeyAS <heretik4@gmail.com>
2020-10-15 14:34:48 +0300

    15.08.20-3

--- resources/views/includes/aside.blade.php
+++
@@ -1,7 +0,0 @@
-@section('aside')
-    <div class="aside">
-        <h4>асайд</h4>
-        <p>это асайд</p>
-        <a class="p-2 text-dark border-dark border about" href="{{ route('about') }}">О Нас</a>
-        @show
-    </div>
--- resources/views/vendor/mail/html/header.blade.php
+++
@@ -1,7 +0,0 @@
-<tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-Laravel Test spam
-</a>
-</td>
-</tr>
